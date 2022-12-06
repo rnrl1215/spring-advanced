a2bf224e86d21d5225beda7fe317ea9455c19cba
skahn <d1215@naver.com>
2022-12-06 13:52:29 +0900

    feat: Add template method

---
+++ src/test/java/hello/advanced/trace/strategy/code/ContextV2Test.java
@@ -0,0 +1,19 @@
+package hello.advanced.trace.strategy.code;
+
+import hello.advanced.trace.strategy.code.strategy.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+}
