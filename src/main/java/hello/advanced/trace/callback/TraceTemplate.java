86ab1e5a3ab0c3395f18ce7947ec0a76f6b15a57
skahn <d1215@naver.com>
2022-12-21 21:25:25 +0900

    feat : Apply template method pattern

---
+++ src/main/java/hello/advanced/trace/callback/TraceTemplate.java
@@ -0,0 +1,26 @@
+package hello.advanced.trace.callback;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+
+public class TraceTemplate {
+    private final LogTrace trace;
+
+    public TraceTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public <T> T excute(String message, TraceCallback<T> callback) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+            T result = callback.call();
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
